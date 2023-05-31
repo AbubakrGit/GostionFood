3463dcab8b82464a090d36c049a83b452154cc75
Abubakir <choynak710@gmail.com>
2023-05-31 10:31:39 +0500

    New Project

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 
-export default class App extends Component {
+export default class Footer extends Component {
   render() {
     return (
-      <>
-      
-      </>
+      <div>Footer</div>
     )
   }
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 
-export default class App extends Component {
+export default class Navbar extends Component {
   render() {
     return (
-      <>
-      
-      </>
+      <div>Navbar</div>
     )
   }
 }
